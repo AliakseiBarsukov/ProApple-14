0552c7c3d9424e287b7b68aa74340b322ec3b71e
Aliaksei Barsukov <barsecky@gmail.com>
2022-09-21 22:03:18 +0300

    feat: add modal window

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+
+// Slider
+
 new Swiper('.goods__block', {
     sliderPerView: 1,
     spaceBetween: 20,
+    loop: true,
     breakpoints: {
         320: {
             slidesPerView: 1,
@@ -23,5 +27,41 @@ new Swiper('.goods__block', {
         nextEl: '.goods__arrow_next',
         prevEl: '.goods__arrow_prev',
     },
+    preventClicks: true,
+    a11y: false,
+});
+
+
+// Modal
+
+const productMore = document.querySelectorAll('.product__more');
+const modal = document.querySelector('.modal');
+
+productMore.forEach(item => {
+    item.addEventListener('click', () => {
+        modal.classList.add('modal_open');
+    })
+});
+
+modal.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target === modal) {
+        modal.classList.remove('modal_open');
+    }
+});
+
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formImput = document.querySelectorAll('.form__input');
+
+
+formImput.forEach((input, i) => {
+    input.addEventListener('focus', () => {
+        formPlaceholder[i].classList.add('form__placeholder_active');
+    })
+    
+    input.addEventListener('blur', () => {
+        if (input.value === '') {
+        formPlaceholder[i].classList.remove('form__placeholder_active');
+        }
+    })
 })
-;
