602d8bf557c39c435bdd7e549c65f96435385cce
Aliaksei Barsukov <barsecky@gmail.com>
2022-11-28 13:23:52 +0300

    feat: add timer or banner

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+// SimpleBar
+
+new SimpleBar(document.querySelector('.country__list'), {
+    classNames: {
+        scrollbar: 'country__scrollbar',
+        track: 'country__track',
+    }
+})
 
 // Slider
 
@@ -43,8 +51,7 @@ productMore.forEach(item => {
     })
 });
 
-modal.addEventListener('click', (event) => {
-    const target = event.target;
+modal.addEventListener('click', ({target}) => {
     if (target === modal) {
         modal.classList.remove('modal_open');
     }
@@ -65,3 +72,128 @@ formImput.forEach((input, i) => {
         }
     })
 })
+
+
+
+// curency
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+    return new Intl.NumberFormat('EU', {
+        style: 'currency',
+        currency, 
+        maximumFractionDigits: 2,
+    }).format(value)
+}
+
+const showPrice = (currency = 'USD') => {
+    const priceElems = document.querySelectorAll('[data-price]');
+
+    priceElems.forEach( elem => {
+        elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency],  currency); 
+    })
+}
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "k32Alm4avKIsf7sRTv1b2Q64nlKuAtLV");
+
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest?base=USD", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        Object.assign(dataCurrency, result.rates)
+        showPrice();
+    })
+    .catch(error => console.log('error', error));
+
+
+
+// choises
+
+const countryBtn = document.querySelector('.country__btn');
+const countryWrapper = document.querySelector('.country__wrapper');
+const countryChoise = document.querySelectorAll('.country__choise');
+
+
+countryBtn.addEventListener('click', () => {
+    countryWrapper.classList.toggle('country__wrapper_open')
+})
+
+countryWrapper.addEventListener('click', ({target}) => {
+    if (target.classList.contains('country__choise')) {
+        countryBtn.textContent = target.dataset.currency;
+        countryWrapper.classList.remove('country__wrapper_open');
+        showPrice(target.dataset.currency)
+
+        // countryChoise.forEach((item, i) => {
+        //     item.addEventListener('click', () => {
+        //         if (countryChoise[i].classList.contains('active')) {
+        //             countryChoise[i].classList.remove('active');
+        //         } else {
+        //             countryChoise[i].classList.add('active');
+        //         }
+        //     });
+        // })
+    }
+});
+
+
+//timer
+
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const timer = dedline => {
+
+    const unitDay = document.querySelector('.timer__unit_day');
+    const unitHour = document.querySelector('.timer__unit_hour');
+    const unitMin = document.querySelector('.timer__unit_min');
+    const descriptionDay = document.querySelector('.timer__unit-description_day');
+    const descriptionHour = document.querySelector('.timer__unit-description_hour');
+    const descriptionMin = document.querySelector('.timer__unit-description_min');
+
+    const getTimeRemaining = () => {
+        const dateStop = new Date(dedline).getTime();
+        const dateNow = Date.now();
+        const timeRemaining = dateStop - dateNow;
+
+        const minutes = Math.floor(timeRemaining / 60 % 60);
+        const hours = Math.floor(timeRemaining / 60 /60 % 24);
+        const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+    
+        return { timeRemaining, minutes, hours, days };
+    }
+
+    const start = () => {
+        const timer = getTimeRemaining();
+
+        unitDay.textContent = timer.days;
+        unitHour.textContent = timer.hours;
+        unitMin.textContent = timer.minutes;
+
+        descriptionDay.textContent = declOfNum(timer.days, ['день', 'дня', 'дней']);
+        descriptionHour.textContent = declOfNum(timer.hours, ['час', 'часа', 'часов']);
+        descriptionMin.textContent = declOfNum(timer.minutes, ['минута', 'минуты', 'минут']);
+
+        const timerId = setTimeout(start, 60000);
+
+        if (timer.timeRemaining < 0) {
+            clearTimeout(timerId)
+            unitDay.textContent = '0';
+            unitHour.textContent = '0';
+            unitMin.textContent = '0';
+        }
+    }
+    start();
+
+}
+
+timer('2023/09/07 20:00')
+
+
